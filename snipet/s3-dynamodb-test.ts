e851c05b3e052a638e1fd42284b1bf8a149479d9
paoo <sntokyo@gmail.com>
2024-09-03 18:09:51 +0900

    add file

--- snipet/s3-dynamodb-test.ts
+++ snipet/s3-dynamodb-test.ts
@@ -6,14 +6,14 @@ import * as path from "path";
 const s3 = new AWS.S3();
 const docClient = new AWS.DynamoDB.DocumentClient();
 const codepipeline = new AWS.CodePipeline();
-
 // 環境変数からテーブル名とバケット名を取得
-const TABLE_NAME = process.env.TABLE_NAME!;
-const BUCKET_NAME = process.env.BUCKET_NAME!;
-const S3_DIR = "s3dir1"; // S3ディレクトリ名を指定
+const TABLE_NAME = process.env.ddbBinarycatalogTableName!;
+const BUCKET_NAME = process.env.s3BucketName!;
+const REPLICATION_BUCKET_NAME = process.env.replicationBucketName!;
+const DEST_BUCKET_NAME_A = process.env.destBucketNameA!;
+const S3_DIR = "s3dir1"; // S3ディレクトリ名を指定（全ての環境で共通）
 
 // DynamoDBの項目の型を定義
-/* eslint-disable @typescript-eslint/naming-convention */
 interface DynamoDBItem {
   pk: string;
   sk: string;
@@ -21,52 +21,66 @@ interface DynamoDBItem {
   datetime: string;
   key: string;
   origkey: string;
-  storedat?: string; // storedatとupdatedatをオプショナルに変更
-  updatedat?: string; // storedatとupdatedatをオプショナルに変更
+  storedat?: string;
+  updatedat?: string;
   value: string;
 }
-/* eslint-enable @typescript-eslint/naming-convention */
 
-// 事前に保持しているデータ
+// テストファイルリスト
+const uploadS3FileList = [
+  "XXXX63_RTGF_010000_202409060000001_001_99999.send",
+];
+
+// 事前に保持しているデータ(DynamoDBに入っていると期待される値)
 const storedData: { [key: string]: DynamoDBItem } = {
-  test1: {
-    pk: "test1",
+  XXXX63_RTGF_010000_202409060000001_001_99999: {
+    pk: "XXXX63_RTGF_010000_202409060000001_001_99999",
     sk: "info",
-    code: "test",
-    datetime: "202307010002291",
-    key: "test1.bin",
-    origkey: "test1.send",
-    storedat: "2024-06-27-T05:59:31Z",
-    updatedat: "2024-06-27-T05:59:31Z",
+    code: "XXXX63",
+    datetime: "202409020002291",
+    key: "XXXX63/XXXX63_RTGF_010000_202409060000001_001_99999.bin",
+    origkey: "danben/test1.send",
+    storedat: "2024-09-02-T05:59:31Z",
+    updatedat: "2024-06-02-T05:59:31Z",
     value: "info",
   },
 };
 
-// オブジェクトのプロパティをソートする関数
 function sortObjectKeys(obj: any): any {
-  if (Array.isArray(obj)) {
-    return obj.map(sortObjectKeys);
-  } else if (obj !== null && typeof obj === 'object') {
-    return Object.keys(obj).sort().reduce((result: any, key: string) => {
-      result[key] = sortObjectKeys(obj[key]);
-      return result;
-    }, {});
+  if (obj !== null && typeof obj === 'object') {
+    const sortedObj: any = {};
+    Object.keys(obj).sort().forEach(key => {
+      sortedObj[key] = obj[key];
+    });
+    return sortedObj;
   } else {
     return obj;
   }
 }
 
-// Lambdaハンドラ関数
+// ファイル名からprefixを取得する関数
+function extractPrefix(filename: string): string | null {
+  const match = filename.match(/^([A-Z0-9]+)_/);
+  return match ? match[1] : null;
+}
+
+// 拡張子が.binのファイルを作成
+function transformFileName(filename: string): string {
+  // 元のファイル名から拡張子を除去し、.binを付与
+  const baseName = filename.replace(/\.[^/.]+$/, "");
+  return `${baseName}.bin`;
+}
+
 export const handler = async (event: any): Promise<void> => {
   const jobId = event["CodePipeline.job"].id;
 
+  // CodePipelineに成功を通知する関数
   const putJobSuccess = async () => {
-    const params = {
-      jobId: jobId
-    };
+    const params = { jobId: jobId };
     await codepipeline.putJobSuccessResult(params).promise();
   };
 
+  // CodePipelineに失敗を通知する関数
   const putJobFailure = async (message: string) => {
     const params = {
       jobId: jobId,
@@ -78,21 +92,20 @@ export const handler = async (event: any): Promise<void> => {
     await codepipeline.putJobFailureResult(params).promise();
   };
 
-  const filenames = ["test1.txt"];
-
+  const filenames = uploadS3FileList;
+  
   const uploadAndCheckFile = async (filename: string) => {
     const filePath = path.join("/tmp", `${filename}`);
 
     // ファイル生成
     fs.writeFileSync(filePath, `This is a test file: ${filename}`);
 
-    // S3にアップロード
+    // S3にアップロード    
     const s3Params = {
       Bucket: BUCKET_NAME,
-      Key: `${S3_DIR}/${filename}`, // S3ディレクトリにアップロード
+      Key: `${S3_DIR}/${filename}`,
       Body: fs.createReadStream(filePath),
     };
-
     await s3.upload(s3Params).promise();
     console.log(`Successfully uploaded ${filename} to S3 directory ${S3_DIR}`);
 
@@ -100,12 +113,15 @@ export const handler = async (event: any): Promise<void> => {
     const maxRetries = 12; // 最大再試行回数
     const delay = 5000; // 再試行間隔（ミリ秒）
     let retries = 0;
-    let found = false;
+    let foundDdb = false;
+    let foundDestBucketA = false;
 
-    const filenameWithoutExtension = path.basename(filename, path.extname(filename)); // ファイル名の拡張子を除去
+    const filenameWithoutExtension = path.basename(filename, path.extname(filename));
+    const prefixDir = extractPrefix(filename);
+    const newFileNameBin = transformFileName(filename);
 
     while (retries < maxRetries) {
-      await new Promise(resolve => setTimeout(resolve, delay)); // 待機
+      await new Promise(resolve => setTimeout(resolve, delay));
 
       // DynamoDBにファイル名が登録されているか確認
       const dynamoParams = {
@@ -116,8 +132,8 @@ export const handler = async (event: any): Promise<void> => {
       const data = await docClient.get(dynamoParams).promise();
       if (data.Item) {
         console.log(`${filenameWithoutExtension} found in DynamoDB`);
-        found = true;
-
+        foundDdb = true;
+        
         // データ付き合わせ
         const storedItem = storedData[filenameWithoutExtension];
         const { storedat: _storedat, updatedat: _updatedat, ...storedItemToCompare } = storedItem;
@@ -131,6 +147,7 @@ export const handler = async (event: any): Promise<void> => {
         if (same) {
           console.log(`Data for ${filenameWithoutExtension} matches stored data.`);
         } else {
+          // マッチしない場合はエラーをスロー
           throw new Error(`Data for ${filenameWithoutExtension} does not match stored data.`);
         }
 
@@ -138,44 +155,62 @@ export const handler = async (event: any): Promise<void> => {
       } else {
         console.log(`${filenameWithoutExtension} not found in DynamoDB, retrying...`);
       }
+
+      // DEST_BUCKET_NAME_Aにファイルが存在するか確認
+      const headParams = {
+        Bucket: DEST_BUCKET_NAME_A,
+        Key: `${prefixDir}/${newFileNameBin}`,
+      };
+
+      try {
+        await s3.headObject(headParams).promise();
+        foundDestBucketA = true;
+        console.log(`File ${newFileNameBin} exists in ${DEST_BUCKET_NAME_A}`);
+        break;
+      } catch (err) {
+        console.error(`File ${newFileNameBin} does not exist in ${DEST_BUCKET_NAME_A}, retrying...`);
+      }
+      
       retries++;
     }
 
-    if (!found) {
+    if (!foundDdb) {
       throw new Error(`Failed to find ${filenameWithoutExtension} in DynamoDB after ${retries} retries.`);
+    }
+    if (!foundDestBucketA) {
+      throw new Error(`Failed to find ${newFileNameBin} in ${DEST_BUCKET_NAME_A} after ${retries} retries.`);
     }
   };
 
   try {
     await Promise.all(filenames.map(uploadAndCheckFile));
-
-    // 成功をCodePipelineに通知
     await putJobSuccess();
     console.log(`Successfully putJobSuccess for ${jobId}`);
-
   } catch (error) {
     console.error(`Test failed with error: ${(error as Error).message}`);
-    // 失敗をCodePipelineに通知
     await putJobFailure((error as Error).message);
     console.log(`Failed as putJobFailure for ${jobId}`);
-
   } finally {
-    // クリーンアップ処理
     for (const filename of filenames) {
       const filenameWithoutExtension = path.basename(filename, path.extname(filename));
+      const prefixDir = extractPrefix(filename);
+      const newFileNameBin = transformFileName(filename);
 
-      // S3からファイルを削除
-      const deleteS3Params = {
-        Bucket: BUCKET_NAME,
-        Key: `${S3_DIR}/${filename}`,
+      //S3bucketからファイルを削除し、ログを出力
+      const deleteFileFromS3 = async (bucket: string, key: string) => {
+        const params = { Bucket: bucket, Key: key };
+        try {
+          await s3.deleteObject(params).promise();
+          console.log(`Successfully deleted ${key} from ${bucket}`);
+        } catch (deleteError) {
+          console.error(`Failed to delete ${key} from ${bucket}: ${(deleteError as Error).message}`);
+        }
       };
 
-      try {
-        await s3.deleteObject(deleteS3Params).promise();
-        console.log(`Successfully deleted ${filename} from S3 directory ${S3_DIR}`);
-      } catch (deleteError) {
-        console.error(`Failed to delete ${filename} from S3: ${(deleteError as Error).message}`);
-      }
+      await deleteFileFromS3(BUCKET_NAME, `${S3_DIR}/${filename}`);
+      await deleteFileFromS3(REPLICATION_BUCKET_NAME, `${S3_DIR}/${filename}`);
+      await deleteFileFromS3(REPLICATION_BUCKET_NAME, `${prefixDir}/${newFileNameBin}`);
+      await deleteFileFromS3(DEST_BUCKET_NAME_A, `${prefixDir}/${newFileNameBin}`);
 
       // DynamoDBからファイル名を削除
       const deleteDynamoParams = {
